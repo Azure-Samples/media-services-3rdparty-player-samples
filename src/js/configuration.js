319fe34368a62b57bb53dc132871622280948ed3
Agustin Knaupp <agustin.knaupp@southworks.com>
2020-09-14 16:21:37 -0300

    Add AVPlayer to the sample list

--- src/js/configuration.js
+++ src/js/configuration.js
@@ -5,7 +5,8 @@ const configuration = {
     { name: 'Shaka', link: 'shaka/index.html', formats: ['DASH', 'HLS'], encryptions: ['CENC', 'CBCS', 'CBC'] },
     { name: 'hls.js', link: 'hls.js/index.html', formats: ['HLS'], encryptions: ['CENC', 'CBC'] },
     { name: 'dash.js', link: 'dash.js/index.html', formats: ['DASH'], encryptions: ['CENC', 'CBC'] },
-    { name: 'ExoPlayer', link: 'azure-samples://exoplayer', formats: ['DASH', 'HLS'], encryptions: ['CENC', 'CBC'], mobile: true }
+    { name: 'ExoPlayer', link: 'azure-samples://exoplayer', formats: ['DASH', 'HLS'], encryptions: ['CENC', 'CBC'], mobile: true },
+    { name: 'AVPlayer', link: 'azure-samples://avplayer', formats: ['HLS'], encryptions: ['CBC', 'CBCS'], mobile: true }
   ]
 }
 
